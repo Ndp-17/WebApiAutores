6f8625d4bb12c951132e21cbe12a0dfea89287c0
Pablo De los Santos <pdelossantos@elcatador.com>
2022-12-10 16:42:31 -0400

    38. Servicios en ASP.NET Core

--- WebApiAutores/Program.cs
+++ WebApiAutores/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System.Text.Json.Serialization;
 using WebApiAutores;
+using WebApiAutores.Servicios;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,13 +12,20 @@ builder.Services.AddControllers().
     AddJsonOptions(x=>x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c => {
+    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+    c.IgnoreObsoleteActions();
+    c.IgnoreObsoleteProperties();
+    c.CustomSchemaIds(type => type.FullName);
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
         )
     );
+builder.Services.AddSingleton<IServicio, ServicioA>();
 
 var app = builder.Build();
 
--- WebApiAutores/Servicios/IServicio.cs
+++ WebApiAutores/Servicios/IServicio.cs
@@ -2,5 +2,30 @@
 {
     public interface IServicio
     {
+        void Realizartarea();
+    }
+    public class ServicioA : IServicio
+    {
+        private readonly ILogger<ServicioA> logger;
+
+        public ServicioA(ILogger<ServicioA> logger)
+        {
+                this.logger = logger;
+        }
+
+        public void Realizartarea()
+        {
+
+        }
+
+    }
+    public class ServicioB : IServicio
+    {
+
+        public void Realizartarea()
+        {
+
+        }
+
     }
 }
