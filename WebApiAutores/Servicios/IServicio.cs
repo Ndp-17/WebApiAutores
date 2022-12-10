25f8c517f95d466ad368a68248b2aec1ad09b0cf
Pablo De los Santos <pdelossantos@elcatador.com>
2022-12-10 16:42:31 -0400

    .

--- WebApiAutores/Servicios/IServicio.cs
+++ WebApiAutores/Servicios/IServicio.cs
@@ -2,5 +2,24 @@
 {
     public interface IServicio
     {
+        void Realizartarea();
+    }
+    public class ServicoA : IServicio
+    {
+
+        public void Realizartarea()
+        {
+
+        }
+
+    }
+    public class ServicoB : IServicio
+    {
+
+        public void Realizartarea()
+        {
+
+        }
+
     }
 }
