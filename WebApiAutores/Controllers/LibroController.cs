15389a3ed4abb916bd7e5aa45cf995e5dbda789f
Pablo De los Santos <pdelossantos@elcatador.com>
2023-04-05 12:00:46 -0400

    Completo 58. Insertando Datos en una Relación Muchos a Muchos

--- WebApiAutores/Controllers/LibroController.cs
+++ WebApiAutores/Controllers/LibroController.cs
@@ -23,30 +23,36 @@ namespace WebApiAutores.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult<LibroDTO>> Get(int id)
         {
-             var libro = await _context.Libros.FirstOrDefaultAsync(x => x.Id == id);
-                                //.Include(x=>x.Comentarios)
+            var libro = await _context.Libros.FirstOrDefaultAsync(x => x.Id == id);
+            //.Include(x=>x.Comentarios)
 
-            if (libro == null)  
-                return NotFound(); 
+            if (libro == null)
+                return NotFound();
 
             return mapper.Map<LibroDTO>(libro);
         }
         [HttpPost]
         public async Task<ActionResult> Post(LibroCreacionDTO libroCreacion)
         {
-            if(libroCreacion.AutoresIds.Any())
+            if (libroCreacion.AutoresIds == null)
                 return BadRequest("No se puede crear un libro sin autores");
 
 
-            var  autoresIds = await _context.Autores.Where(at=> libroCreacion.AutoresIds.
-                                        Contains(at.Id)).Select(x=>x.Id).ToListAsync();
-            if(autoresIds.Count != libroCreacion.AutoresIds.Count) 
-            { 
+            var autoresIds = await _context.Autores.Where(at => libroCreacion.AutoresIds.
+                                        Contains(at.Id)).Select(x => x.Id).ToListAsync();
+            if (autoresIds.Count != libroCreacion.AutoresIds.Count)
+            {
                 return BadRequest("No exite uno de los autores enviados.");
-            
+
             }
 
             var libro = mapper.Map<Libro>(libroCreacion);
+
+            for (int i = 0; i < libro.AutoresLibros.Count; i++)
+            {
+                libro.AutoresLibros[i].Orden = i;
+
+            }
             _context.Add(libro);
             await _context.SaveChangesAsync();
             return Ok();
