a9f3564ac9cef8ed44a11f783236c1bdfb50eb05
Pablo De los Santos <pdelossantos@elcatador.com>
2023-05-08 16:45:44 -0400

    79. Funcionalidad de Registro de Usuarios

---
+++ WebApiAutores/Controllers/CuentasController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using WebApiAutores.DTOs;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/cuentas")]
+    public class CuentasController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IConfiguration configuration;
+        public CuentasController(UserManager<IdentityUser> userManager,
+                                 IConfiguration configuration) 
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+        
+        }
+
+
+
+        [HttpPost("registrar")]
+        public async Task<AcceptedResult<RespuestaAutenticacion>> Registrar(CredencialesUsuario credencialesUsuario)
+        {
+            var usario = new IdentityUser { UserName = credencialesUsuario.Email, Email = credencialesUsuario.Email };
+            var resultado = await userManager.CreateAsync(usario,credencialesUsuario.Password);
+
+            if(resultado.Succeeded) 
+            { 
+
+            }
+            else
+            {
+                return BadRequest(resultado.Errors);
+            }
+                
+        }
+
+
+        private RespuestaAutenticacion ConstruirToken(CredencialesUsuario credencialesUsuario)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("email",credencialesUsuario.Email),
+                new Claim("Niels","Niels")
+
+            };
+
+            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration[]));
+        
+        }
+     
+    }
+}
